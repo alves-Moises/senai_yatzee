6649733f4e2a2aa6d99421e90681ef453dffe78a
alves-Moises <moisesinho_ra@hotmail.com>
2025-08-06 12:49:34 -0300

    update dices struct

--- src/functions/dices.js
+++ src/functions/dices.js
@@ -1,11 +1,12 @@
 
-export const  dicesImg = {
-    "1": "https://lh3.googleusercontent.com/pw/AP1GczN3OfIwKEE9xAX24Q0mzeMsd4ed5mVWZGqd095Dtgcte3D4OQRqLQwt-uaUnK7dfcYVuI2gpL7Rh9k48AhFlNkfsnEfZP37hp46aLJQSs78mB5WfYUU97sciPBMHvNV9l0Ims3z2exvg8VBbIoZBnfGSw=w993-h993-s-no-gm",
-    "2": "https://photos.fife.usercontent.google.com/pw/AP1GczOAHtHgW8ue66nkFb23wNP8pHMCI14CulwYzo8DAuzSFXuoaLvG45EIXA=w661-h661-s-no-gm?authuser=0",
-    "3": "https://photos.fife.usercontent.google.com/pw/AP1GczOKMp6lf9xWsUmA944-tOMekFYyQI5uj1uKu0M3H3uie0Mg8Xe3BEcx1g=w661-h661-s-no-gm?authuser=0", 
-    "4": "https://photos.fife.usercontent.google.com/pw/AP1GczMhRWbuK6-mKINhyKwBluC1u_JWO8fuu9UOgSZULyxqGU6C5OsYnFnP6A=w661-h661-s-no-gm?authuser=0",
-    "5": "https://photos.fife.usercontent.google.com/pw/AP1GczNT1ZCfRXCwAhv-aiV7HWBN2JE4fQcXwcAGUI5I4bhtIkbM3m1kGI5uvA=w661-h661-s-no-gm?authuser=0",
-    "6": "https://photos.fife.usercontent.google.com/pw/AP1GczP8Ej_LhETo4L8twQz7dJymrPx-Rv0VAma_DqIsgeD4gZRoGFtcT3Wt7Q=w661-h661-s-no-gm?authuser=0"
+export const dicesImg  = {
 
+
+    "1": "https://lh3.googleusercontent.com/pw/AP1GczN3OfIwKEE9xAX24Q0mzeMsd4ed5mVWZGqd095Dtgcte3D4OQRqLQwt-uaUnK7dfcYVuI2gpL7Rh9k48AhFlNkfsnEfZP37hp46aLJQSs78mB5WfYUU97sciPBMHvNV9l0Ims3z2exvg8VBbIoZBnfGSw=w993-h993-s-no-gm",
+    "2": "https://lh3.googleusercontent.com/pw/AP1GczOhXpFBi4taHThF85yJw7xLmX4LknXI-q9PrXybkzkD65bgS8seMS_1KdTHRegr1pGCPH4xOTjejE0xt2_IXsQ83Hmd9ZwDnAujoYF-2W_J2oCvUh1q_sOLVtVwaZZJ1xMWeIi74t7MLp3DCMFwMkqHag=w907-h907-s-no-gm",
+    "3": "https://lh3.googleusercontent.com/pw/AP1GczOH_fDhy3hN0Gb3MdsjEc_XC3GjL4jY2NqZMIhm4N0iJ4SW73RnECiKkbmugwQXGD17LKI-rurdYyLkXvIMRZYJNeMi1XF44teWanKFYrL3NmT7uXk509utlhcGKkLbCRSpWlN_87gaBxWsaeewZoFCRg=w907-h907-s-no-gm", 
+    "4": "https://lh3.googleusercontent.com/pw/AP1GczO21U3SN2VRmx2ZuzY2a4PMWtkoDP-A3FQsMkwpN1XqWbcvuNmJarKqGx11yHFMTo68_3cqNuMj326IXUovRVYovfn76iAJq4LELj5t0DoLEGHCgBZQNrKaEm8rc7Z1nrLEJUPDzy2ozLpkKhHAlNaZlA=w907-h907-s-no-gm",
+    "5": "https://lh3.googleusercontent.com/pw/AP1GczNd4P-9pZkCKX77Y1xcAOcFwaLwhIIvCH_8MjDa9B3FrgWK-mvF4S_xcMxFxowmF-dWJNJd0qTK2YjICXWmSiFmGnZrIDfeLM0BAGE5sdNErHfXWtpzct_klVhKXwKXiUb6vHalITxL5DM1glNy_pKwfw=w907-h907-s-no-gm",
+    "6": "https://lh3.googleusercontent.com/pw/AP1GczOveqThPU1uCaXRpU1kFkvKhDPVzpkWefK3a57TZfOp90bkWzQ3tiGausogZkoL5J3FRgQTbxNxc2Viqckk0fAZX_4F0pB0JQy5D2QkuJZuXy5PA2QJWDEzZcmwNTIx0BQ1w0_Umym_ulaa449IYwXc4Q=w907-h907-s-no-gm"
 }
 
